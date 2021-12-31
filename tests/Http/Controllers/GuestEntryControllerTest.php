9a18d582c674992fd1f850b21dc234474a09c745
Duncan McClean <duncan@mcclean.co.uk>
2021-12-31 16:14:47 +0000

    wip

--- tests/Http/Controllers/GuestEntryControllerTest.php
+++ tests/Http/Controllers/GuestEntryControllerTest.php
@@ -37,15 +37,12 @@ class GuestEntryControllerTest extends TestCase
     {
         Collection::make('comments')->save();
 
-        $response = $this
+        $this
             ->post(route('statamic.guest-entries.store'), [
                 '_collection' => 'comments',
                 'title' => 'This is great',
                 'slug' => 'this-is-great',
             ]);
-            // ->assertRedirect();
-
-        dd($response);
 
         $entry = Entry::all()->last();
 
