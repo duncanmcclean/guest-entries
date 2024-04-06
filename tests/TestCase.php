d0b1cb8583460ca1e144dd07a017f47b43ecea9e
Duncan McClean <duncan@duncanmcclean.com>
2024-04-06 18:31:22 +0100

    Simplify `TestCase`

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,53 +3,9 @@
 namespace DuncanMcClean\GuestEntries\Tests;
 
 use DuncanMcClean\GuestEntries\ServiceProvider;
-use Orchestra\Testbench\TestCase as OrchestraTestCase;
-use Statamic\Extend\Manifest;
-use Statamic\Providers\StatamicServiceProvider;
-use Statamic\Statamic;
+use Statamic\Extend\AddonTestCase;
 
-abstract class TestCase extends OrchestraTestCase
+abstract class TestCase extends AddonTestCase
 {
-    protected function getPackageProviders($app)
-    {
-        return [
-            StatamicServiceProvider::class,
-            ServiceProvider::class,
-        ];
-    }
-
-    protected function getPackageAliases($app)
-    {
-        return [
-            'Statamic' => Statamic::class,
-        ];
-    }
-
-    protected function getEnvironmentSetUp($app)
-    {
-        parent::getEnvironmentSetUp($app);
-
-        $app->make(Manifest::class)->manifest = [
-            'duncanmcclean/guest-entries' => [
-                'id' => 'duncanmcclean/guest-entries',
-                'namespace' => 'DuncanMcClean\\GuestEntries',
-            ],
-        ];
-    }
-
-    protected function resolveApplicationConfiguration($app)
-    {
-        parent::resolveApplicationConfiguration($app);
-
-        $configs = [
-            'assets', 'cp', 'forms', 'static_caching',
-            'sites', 'stache', 'system', 'users',
-        ];
-
-        foreach ($configs as $config) {
-            $app['config']->set("statamic.$config", require (__DIR__."/../vendor/statamic/cms/config/{$config}.php"));
-        }
-
-        $app['config']->set('statamic.users.repository', 'file');
-    }
+    protected string $addonServiceProvider = ServiceProvider::class;
 }
