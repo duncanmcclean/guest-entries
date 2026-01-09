4c08b1f31170870b6dabe5f9c616d82e75a9330d
Duncan McClean <duncan@duncanmcclean.com>
2026-01-09 17:07:18 +0000

    require alpha 21

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,14 +8,4 @@ use Statamic\Testing\AddonTestCase;
 abstract class TestCase extends AddonTestCase
 {
     protected string $addonServiceProvider = ServiceProvider::class;
-
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        // We need to do this until https://github.com/statamic/cms/pull/13396
-        // has been merged and tagged.
-        \Statamic\Facades\CP\Nav::shouldReceive('clearCachedUrls')->zeroOrMoreTimes();
-        $this->addToAssertionCount(-1); // Dont want to assert this
-    }
 }
